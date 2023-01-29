e04292a260b2921f7a0b69521acc7d9f26b45c50
Imam Firmansyah <imam.firmansyah@yesdok.com>
2023-01-29 13:31:55 +0700

    just for task

---
+++ index.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+
+        <!--====== Title ======-->
+        <title>Home</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--====== Favicon Icon ======-->
+        <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+        <!--====== Slick CSS ======-->
+        <link rel="stylesheet" href="assets/css/slick.css">
+
+        <!--====== Font Awesome CSS ======-->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!--====== Line Icons CSS ======-->
+        <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+        <!--====== Animate CSS ======-->
+        <link rel="stylesheet" href="assets/css/animate.css">
+
+        <!--====== Magnific Popup CSS ======-->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+        <!--====== Bootstrap CSS ======-->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!--====== Default CSS ======-->
+        <link rel="stylesheet" href="assets/css/default.css">
+
+        <!--====== Style CSS ======-->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+    </head>
+
+    <body>
+        <!--[if IE]> <p class="browserupgrade">You are using an
+        <strong>outdated</strong> browser. Please <a
+        href="https://browsehappy.com/">upgrade your browser</a> to improve your
+        experience and security.</p> <![endif]-->
+
+        <!--====== PRELOADER PART START ======-->
+
+        <div class="preloader">
+            <div class="loader">
+                <div class="ytp-spinner">
+                    <div class="ytp-spinner-container">
+                        <div class="ytp-spinner-rotator">
+                            <div class="ytp-spinner-left">
+                                <div class="ytp-spinner-circle"></div>
+                            </div>
+                            <div class="ytp-spinner-right">
+                                <div class="ytp-spinner-circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--====== PRELOADER PART ENDS ======-->
+
+        <!--====== HEADER PART START ======-->
+
+        <header class="header-area">
+            <div class="navbar-area headroom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <nav class="navbar navbar-expand-lg">
+                                <button
+                                    class="navbar-toggler"
+                                    type="button"
+                                    data-toggle="collapse"
+                                    data-target="#navbarSupportedContent"
+                                    aria-controls="navbarSupportedContent"
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                </button>
+
+                                <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                    <ul id="nav" class="navbar-nav m-auto">
+                                        <li class="nav-item active">
+                                            <a href="#home">Home</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#about">About
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#contact">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- navbar collapse -->
+                            </nav>
+                            <!-- navbar -->
+                        </div>
+                    </div>
+                    <!-- row -->
+                </div>
+                <!-- container -->
+            </div>
+            <!-- navbar area -->
+
+            <div
+                id="home"
+                class="header-hero bg_cover d-lg-flex align-items-center"
+                style="background-image: url(assets/images/header-hero.jpg)">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="header-hero-content">
+                                <h1
+                                    class="hero-title wow fadeInUp"
+                                    data-wow-duration="1s"
+                                    data-wow-delay="0.2s">
+                                    <b>Welcome,</b><br>
+                                    <span>Future</span>
+                                    <b>Learning.</b>
+                                </h1>
+                                <p class="text wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">Mewujudkan
+                                    Sekolah Tinggi Yang Unggul Dalam Penyelenggaraan Tridharma Perguruan Tinggi Di
+                                    Bidang Manajemen Dan Akuntansi Tingkat Nasional Tahun 2030.</p>
+                                <div
+                                    class="header-singup wow fadeInUp"
+                                    data-wow-duration="1s"
+                                    data-wow-delay="0.8s">
+                                    <input type="text" placeholder="username@yourdomain.com">
+                                    <button class="main-btn">Sign Up</button>
+                                </div>
+                            </div>
+                            <!-- header hero content -->
+                        </div>
+                    </div>
+                    <!-- row -->
+                </div>
+                <!-- container -->
+                <div
+                    class="header-hero-image d-flex align-items-center wow fadeInRightBig"
+                    data-wow-duration="1s"
+                    data-wow-delay="1.1s">
+                </div>
+                <!-- header hero image -->
+            </div>
+            <!-- header hero -->
+        </header>
+
+        <!--====== HEADER PART ENDS ======-->
+
+        <!--====== ABOUT PART START ======-->
+
+        <section id="about" class="about-area pt-115">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <div
+                            class="about-title text-center wow fadeInUp"
+                            data-wow-duration="1s"
+                            data-wow-delay="0.3s">
+                            <h6 class="welcome">STIE Dharma Negara</h6>
+                            <h3 class="title">
+                                <span>Visi Prodi Manajemen
+                                </span>
+                                “Menjadi Program Studi Manajemen yang unggul pada tingkat nasional tahun2030”.</h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- row -->
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="about-content pt-45">
+                            <p class="text wow fadeInUp text-center" data-wow-duration="1s" data-wow-delay="0.4s">Buat Kamu yang akan masuk kuliah tahun ini dan berencana mengambil jurusan ilmu ekonomi, Kamu juga perlu tahu nih prospek kerja ilmu ekonomi setelah lulus nanti. Dengan begitu ketika nanti ketemu tante, budhe, om, pakdhe, atau bahkan calon mertua, bisa jawab ketika mereka bertanya “jurusan Kamu nanti kerjanya jadi apa?”.</p>
+                        </div>
+                        <!-- about content -->
+                    </div>
+                </div>
+                <!-- row -->
+            </div>
+            <!-- container -->
+        </section>
+
+        <!--====== ABOUT PART ENDS ======-->
+
+        <!--====== MYSQL INSERT START ======-->
+
+        <?php
+            if(isset($_POST['Submit'])) {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $message = $_POST['message'];
+                
+                // include database connection file
+                include_once("config.php");
+                        
+                // Insert user data into table
+                $result = mysqli_query($mysqli, "INSERT INTO help_page(name,email,message) VALUES('$name','$email','$message')");
+            }
+        ?>
+
+        <!--====== MYSQL INSERT END ======-->
+
+        <!--====== CONTACT PART START ======-->
+
+        <section id="contact" class="contact-area pt-120 pb-120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div
+                            class="contact-wrapper-form pt-10 wow fadeInUpBig"
+                            data-wow-duration="1s"
+                            data-wow-delay="0.5s">
+                            <h4 class="contact-title pb-10">
+                                <i class="lni-envelope"></i>
+                                Leave
+                                <span>A Message.</span></h4>
+
+                            <form action="index.php" id="contact-form" method="post">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="contact-form mt-45">
+                                            <label>Enter Your Name</label>
+                                            <input type="text" name="name" placeholder="Full Name">
+                                        </div>
+                                        <!-- contact-form -->
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="contact-form mt-45">
+                                            <label>Enter Your Email</label>
+                                            <input type="email" name="email" placeholder="Email">
+                                        </div>
+                                        <!-- contact-form -->
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="contact-form mt-45">
+                                            <label>Your Message</label>
+                                            <textarea name="message" placeholder="Enter your message..."></textarea>
+                                        </div>
+                                        <!-- contact-form -->
+                                    </div>
+                                    <p class="form-message"></p>
+                                    <div class="col-md-3">
+                                        <div class="contact-form mt-45">
+                                            <input type="submit" name="Submit" value="Send Now" class="btn main-btn" id="submit-message" >
+                                        </div>
+                                        <!-- contact-form -->
+                                    </div>
+                                </div>
+                                <!-- row -->
+                            </form>
+                        </div>
+                        <!-- contact wrapper form -->
+                    </div>
+                </div>
+                <!-- row -->
+            </div>
+            <!-- container -->
+        </section>
+
+        <!--====== CONTACT PART ENDS ======-->
+
+        <!--====== BACK TOP TOP PART START ======-->
+
+        <a href="#" class="back-to-top">
+            <i class="lni-chevron-up"></i>
+        </a>
+
+
+        <!--====== BACK TOP TOP PART ENDS ======-->
+
+        <!--====== Jquery js ======-->
+        <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+
+        <!--====== Bootstrap js ======-->
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+
+        <!--====== Slick js ======-->
+        <script src="assets/js/slick.min.js"></script>
+
+        <!--====== Isotope js ======-->
+        <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+        <script src="assets/js/isotope.pkgd.min.js"></script>
+
+        <!--====== Counter Up js ======-->
+        <script src="assets/js/waypoints.min.js"></script>
+        <script src="assets/js/jquery.counterup.min.js"></script>
+
+        <!--====== Circles js ======-->
+        <script src="assets/js/circles.min.js"></script>
+
+        <!--====== Appear js ======-->
+        <script src="assets/js/jquery.appear.min.js"></script>
+
+        <!--====== WOW js ======-->
+        <script src="assets/js/wow.min.js"></script>
+
+        <!--====== Headroom js ======-->
+        <script src="assets/js/headroom.min.js"></script>
+
+        <!--====== Jquery Nav js ======-->
+        <script src="assets/js/jquery.nav.js"></script>
+
+        <!--====== Scroll It js ======-->
+        <script src="assets/js/scrollIt.min.js"></script>
+
+        <!--====== Magnific Popup js ======-->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+        <!--====== Main js ======-->
+        <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
